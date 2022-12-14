b90cdf87ef1523969a1dcc895b63262330650d03
Hector Tanabe <hector.tanabe@jalasoft.com>
2022-12-14 18:30:44 -0400

    Second-Class

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,17 +1,31 @@
 import pygame
+import random
 
 from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
   def __init__(self):
     self.obstacles = []
     
+  def generate_obstacle(self, obstacle_type):
+    if obstacle_type == 0:
+      cactus_type = 'SMALL'
+      obstacle = Cactus(cactus_type)
+    elif obstacle_type == 1:
+      cactus_type = 'LARGE'
+      obstacle = Cactus(cactus_type)
+    else:
+      obstacle = Bird()
+    return obstacle
+    
   def update(self, game):
     if len(self.obstacles) == 0:
-      cactus = Cactus(SMALL_CACTUS)
-      self.obstacles.append(cactus)
+      obstacle_type = random.randint(0, 2)
+      obstacle = self.generate_obstacle(obstacle_type)
+      self.obstacles.append(obstacle)
       
     for obstacle in self.obstacles:
       obstacle.update(game.game_speed, self.obstacles)
